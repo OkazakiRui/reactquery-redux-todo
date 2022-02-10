c21fe1de67f856b44173a424eedf274d7f90c573
RuiOkazaki <okazaki200226@gmail.com>
2022-02-11 00:54:13 +0900

    update: react-query と devtools を使えるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import { VFC } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { BrowserRouter } from 'react-router-dom'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 const App: VFC = () => {
-  return <div></div>
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono">
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter></BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </div>
+  )
 }
 
 export default App
