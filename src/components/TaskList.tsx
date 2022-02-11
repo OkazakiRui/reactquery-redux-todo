1ff4ccbeeaffeec39075c1429c217945115d29c8
RuiOkazaki <okazaki200226@gmail.com>
2022-02-11 09:32:14 +0900

    add: Task一覧を表示するcomponentを作成した

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,19 @@
+import { useQueryTasks } from 'hooks/useQueryTasks'
+import { VFC } from 'react'
+import { TaskItem } from './TaskItem'
+
+export const TaskList: VFC = () => {
+  console.log('TaskList がレンダリングされた')
+
+  const { status, data } = useQueryTasks()
+  if (status === 'loading') return <div>loading...</div>
+  if (status === 'error') return <div>error</div>
+
+  return (
+    <ul>
+      {data?.map((task) => (
+        <TaskItem task={task} />
+      ))}
+    </ul>
+  )
+}
