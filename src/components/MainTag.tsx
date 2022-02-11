c54cc790261586351d6a59998934fb9f23a870de
RuiOkazaki <okazaki200226@gmail.com>
2022-02-12 00:22:47 +0900

    update: taglist, tagedit componentを読み込んだ

--- src/components/MainTag.tsx
+++ src/components/MainTag.tsx
@@ -1,6 +1,8 @@
 import { VFC } from 'react'
 import { useHistory } from 'react-router-dom'
 import { ChevronDoubleLeftIcon } from '@heroicons/react/solid'
+import { TagList } from 'components/TagList'
+import { TagEdit } from 'components/TagEdit'
 
 export const MainTag: VFC = () => {
   console.log('MainTag がレンダリングされた')
@@ -10,7 +12,10 @@ export const MainTag: VFC = () => {
   return (
     <>
       <p className="mb-10 text-xl font-bold">Tags</p>
-      <div className="grid grid-cols-2 gap-40"></div>
+      <div className="grid grid-cols-2 gap-40">
+        <TagList />
+        <TagEdit />
+      </div>
       <ChevronDoubleLeftIcon
         onClick={toTaskPage}
         className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
